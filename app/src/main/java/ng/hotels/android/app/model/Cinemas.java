757072af98a5e8f1fca1aa9363a131f952e3e843
lanre01 <olanre12waju@gmail.com>
2017-10-07 15:07:14 +0100

    Add code for the adapter for Events and for Cinemas

--- app/src/main/java/ng/hotels/android/app/model/Cinemas.java
+++ app/src/main/java/ng/hotels/android/app/model/Cinemas.java
@@ -5,4 +5,54 @@ package ng.hotels.android.app.model;
  */
 
 public class Cinemas {
+
+    private String cinemaName;
+
+    private String cinemaImageUrl;
+
+    private String time;
+
+    private String date;
+
+    public Cinemas() {
+    }
+
+    public Cinemas(String cinemaName, String cinemaImageUrl, String time, String date) {
+        this.cinemaName = cinemaName;
+        this.cinemaImageUrl = cinemaImageUrl;
+        this.time = time;
+        this.date = date;
+    }
+
+    public String getCinemaName() {
+        return cinemaName;
+    }
+
+    public void setCinemaName(String cinemaName) {
+        this.cinemaName = cinemaName;
+    }
+
+    public String getCinemaImageUrl() {
+        return cinemaImageUrl;
+    }
+
+    public void setCinemaImageUrl(String cinemaImageUrl) {
+        this.cinemaImageUrl = cinemaImageUrl;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
