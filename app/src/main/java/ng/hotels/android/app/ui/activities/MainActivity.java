a4f54e7736d7f9e363f4036d2c123c7268ac04c2
Arinze39 <arinzenne2017@gmail.com>
2017-10-08 16:13:11 +0100

    Added ATM Finder Page

--- app/src/main/java/ng/hotels/android/app/ui/activities/MainActivity.java
+++ app/src/main/java/ng/hotels/android/app/ui/activities/MainActivity.java
@@ -180,6 +180,11 @@ public class MainActivity extends AppCompatActivity implements
         startActivity(intent);
     }
 
+    public void openATMFinder(View view) {
+        Intent intent = new Intent(getApplicationContext(), ATMFinder_Activity.class);
+        startActivity(intent);
+    }
+
     @Override
     public void onConfirmClicked() {
 
