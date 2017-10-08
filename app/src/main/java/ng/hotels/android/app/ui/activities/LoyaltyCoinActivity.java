a4e24a7ddc6c71247e2c3799f2b5d3bf6a6c78c3
teejay13 <adetunjisalako@gmail.com>
2017-10-08 12:24:36 +0000

    button added loyalty coin

--- app/src/main/java/ng/hotels/android/app/ui/activities/LoyaltyCoinActivity.java
+++ app/src/main/java/ng/hotels/android/app/ui/activities/LoyaltyCoinActivity.java
@@ -2,6 +2,7 @@ package ng.hotels.android.app.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import ng.hotels.android.app.R;
 
@@ -11,6 +12,8 @@ public class LoyaltyCoinActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_loyalty_coin);
     }
 }
